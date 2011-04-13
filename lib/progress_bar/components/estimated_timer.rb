a8ea4c9bb22e21258699b5b1f7ff2a0399a373c3
jfelchner <jeff@thekompanee.com>
2011-04-13 11:32:39 -0500

    Estimated Timer works with out of bounds times

--- lib/progress_bar/components/estimated_timer.rb
+++ lib/progress_bar/components/estimated_timer.rb
@@ -1,6 +1,8 @@
 module ProgressBar
   module Components
     class EstimatedTimer
+      VALID_OOB_TIME_FORMATS = [:unknown, :friendly]
+
       def initialize(options = {})
         @current = options[:current] || 0
         @total   = options[:total]   || raise("You have to pass the total capacity to find an estimate.")
@@ -14,6 +16,14 @@ module ProgressBar
         @current += 1 unless current == total
       end
 
+      def out_of_bounds_time_format=(format)
+        if VALID_OOB_TIME_FORMATS.include? format
+          @out_of_bounds_time_format = format
+        else
+          raise "Invalid Out Of Bounds time format.  Valid formats are #{VALID_OOB_TIME_FORMATS.inspect}"
+        end
+      end
+
       def to_s
         " ETA: #{estimated_time}"
       end
@@ -24,7 +34,11 @@ module ProgressBar
 
           hours, minutes, seconds = divide_seconds(estimated_seconds_remaining)
 
-          hours <= 99 ? sprintf("%02d:%02d:%02d", hours, minutes, seconds) : "> 4 Days"
+          if hours > 99 && @out_of_bounds_time_format
+            out_of_bounds_time
+          else
+            sprintf "%02d:%02d:%02d", hours, minutes, seconds
+          end
         end
 
         def elapsed_seconds
@@ -44,6 +58,15 @@ module ProgressBar
           minutes, seconds = seconds.divmod(60)
 
           [hours, minutes, seconds]
+        end
+
+        def out_of_bounds_time
+          case @out_of_bounds_time_format
+          when :unknown
+            '??:??:??'
+          when :friendly
+            '> 4 Days'
+          end
         end
     end
   end
--- spec/progress_bar/components/estimated_timer_spec.rb
+++ spec/progress_bar/components/estimated_timer_spec.rb
@@ -22,5 +22,44 @@ describe ProgressBar::Components::EstimatedTimer do
         end
       end
     end
+
+    context "when it's estimated to take longer than 99:59:59" do
+      before do
+          @estimated_time = ProgressBar::Components::EstimatedTimer.new(:current => 25, :total => 100)
+
+          Timecop.travel(-120000) do
+            @estimated_time.start
+          end
+      end
+
+      context "and the out of bounds time format has been set to 'friendly'" do
+        before { @estimated_time.out_of_bounds_time_format = :friendly }
+
+        it "displays '> 4 Days' remaining" do
+          @estimated_time.to_s.should eql " ETA: > 4 Days"
+        end
+      end
+
+      context "and the out of bounds time format has been set to 'unknown'" do
+        before { @estimated_time.out_of_bounds_time_format = :unknown }
+
+        it "displays '??:??:??' remaining" do
+          @estimated_time.to_s.should eql " ETA: ??:??:??"
+        end
+      end
+
+      it "displays the correct time remaining" do
+        @estimated_time.to_s.should eql " ETA: 100:00:00"
+      end
+    end
+  end
+
+  describe "#out_of_bounds_time_format=" do
+    context "when set to an invalid format" do
+      it "raises an exception" do
+        @estimated_time = ProgressBar::Components::EstimatedTimer.new(:total => 100)
+        lambda{ @estimated_time.out_of_bounds_time_format = :foo }.should raise_error("Invalid Out Of Bounds time format.  Valid formats are [:unknown, :friendly]")
+      end
+    end
   end
 end
