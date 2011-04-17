e538bd633a7888a43d32e52c95de1536d6c3f063
jfelchner <jeff@thekompanee.com>
2011-04-17 15:29:43 -0500

    Update spec_helper with new root gem file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ SimpleCov.start
 
 require 'rspec'
 
-Dir[File.join(File.dirname(__FILE__), "..", "lib", "progress_bar.rb")].each {|f| require f}
+Dir[File.join(File.dirname(__FILE__), "..", "lib", "ruby-progressbar.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "..", "spec", "support", "**", "*.rb")].each {|f| require f}
 
 Rspec.configure do |c|
