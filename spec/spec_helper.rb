c69e33181cc8ac24f053d23e3c2b632d0fc1278c
Jeff Felchner <jeff@thekompanee.com>
2012-01-21 18:40:36 -0600

    Upgraded RSpec uses RSpec not Rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,6 @@ require 'rspec'
 Dir[File.join(File.dirname(__FILE__), "..", "lib", "ruby-progressbar.rb")].each {|f| require f}
 Dir[File.join(File.dirname(__FILE__), "..", "spec", "support", "**", "*.rb")].each {|f| require f}
 
-Rspec.configure do |c|
+RSpec.configure do |c|
   c.mock_with :rspec
 end
