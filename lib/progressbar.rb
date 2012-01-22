15c69f163e7d3489a01bdcdf1abdbb46e08ee33a
Jeff Felchner <jeff@thekompanee.com>
2012-01-21 18:40:48 -0600

    Change to new date and repo

--- lib/progressbar.rb
+++ lib/progressbar.rb
@@ -4,5 +4,5 @@
 # `ruby-progressbar.rb` because the gem name should match the filename under `lib` and `progressbar.rb` because this gem was copied
 # directly from another gem and wasn't properly modified.  There are other applications which require this file.
 #
-puts "DEPRECATION WARNING: This file will be removed on or after October 30th, 2011.  You should modify your Gemfiles and/or require lines to utilize the new syntax here: https://github.com/thekompanee/ruby-progressbar."
+puts "DEPRECATION WARNING: This file will be removed on or after October 30th, 2012.  You should modify your Gemfiles and/or require lines to utilize the new syntax here: https://github.com/jfelchner/ruby-progressbar."
 require File.join(File.dirname(__FILE__), "ruby-progressbar")
