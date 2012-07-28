d048824cf35ddef76d1161923b089a584475d4e2
Jeff Felchner <accounts+git@thekompanee.com>
2012-07-28 15:46:39 -0500

    Update the deprecation date of

--- lib/progress_bar/base.rb
+++ lib/progress_bar/base.rb
@@ -48,7 +48,7 @@ module ProgressBar
     end
 
     def inc
-      puts 'DEPRECATION WARNING: #inc will be removed on or after October 30th, 2012.  Please use #increment'
+      puts 'DEPRECATION WARNING: #inc will be removed on or after October 30th, 2013.  Please use #increment'
       increment
     end
 
@@ -65,7 +65,7 @@ module ProgressBar
     end
 
     def set(new_value)
-      puts 'DEPRECATION WARNING: #set will be removed on or after October 30th, 2012.  Please use #progress='
+      puts 'DEPRECATION WARNING: #set will be removed on or after October 30th, 2013.  Please use #progress='
       progress = new_value
     end
 
@@ -87,7 +87,7 @@ module ProgressBar
     end
 
     def halt
-      puts 'DEPRECATION WARNING: #halt will be removed on or after October 30th, 2012.  Please use #stop'
+      puts 'DEPRECATION WARNING: #halt will be removed on or after October 30th, 2013.  Please use #stop'
       stop
     end
 
@@ -128,13 +128,13 @@ module ProgressBar
   private
     attr_accessor   :output
 
-    # This will be removed on or after October 30th, 2011 and is only here to provide backward
+    # This will be removed on or after October 30th, 2013 and is only here to provide backward
     # compatibility with the previous versions of ruby-progressbar.
     def backwards_compatible_args_to_options_conversion(args)
       options = {}
 
       if args.size > 1
-        puts 'DEPRECATION WARNING: Creating Progress Bars in this way has been deprecated and will be removed on or after October 30th, 2012.  Please update your code to use the new initializer syntax found here: https://github.com/jfelchner/ruby-progressbar.'
+        puts 'DEPRECATION WARNING: Creating Progress Bars in this way has been deprecated and will be removed on or after October 30th, 2013.  Please update your code to use the new initializer syntax found here: https://github.com/jfelchner/ruby-progressbar.'
         options[:title]  = args[0]
         options[:total]  = args[1]
         options[:output] = args[2]
