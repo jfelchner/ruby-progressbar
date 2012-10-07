92355e29ab53091960071eee18c3ee8efe397506
Jeff Felchner <accounts+git@thekompanee.com>
2012-10-07 18:30:18 -0500

    Hashes are not ordered and therefore when looking for the time mocking method, we weren't selecting the proper one. Switched to an Array instead.

--- lib/progress_bar/time.rb
+++ lib/progress_bar/time.rb
@@ -8,15 +8,15 @@ class ProgressBar
 
   private
     def self.unmocked_time_method
-      time_mocking_library_mapping.values.find { |method| @@time.respond_to? method }
+      time_mocking_library_methods.find { |method| @@time.respond_to? method }
     end
 
-    def self.time_mocking_library_mapping
-      {
-        :timecop  => :now_without_mock_time,
-        :delorean => :now_without_delorean,
-        :actual   => :now
-      }
+    def self.time_mocking_library_methods
+      [
+        :now_without_mock_time,       # Timecop
+        :now_without_delorean,        # Delorean
+        :now                          # Actual
+      ]
     end
   end
 end
