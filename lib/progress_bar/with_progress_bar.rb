7d416b60d1697f9ff8a02a7c11c9f262518453c6
Florian Frank <flori@ping.de>
2013-05-02 11:20:15 +0200

    Implement with_progress_bar convenience method
    Now this
    
        (1..10000).with_progress_bar(format: '%t: |%w>%i| (%c/%C) %e').to_a
    
    displays a progress bar with a total of 10,000 without having to
    manually increment the progress. Passing options like :format is still
    possible as shown in the example. Accessing the progress bar instance is
    possible via the thread local variable progress_bar, where
    progress_bar.skip can be used to skip incrementation of the progress and
    continue with the next iteration.
    
    The module containing with_progress_bar is mixed into Enumerable and all
    Enumerable containing modules, when
    
        require 'progress_bar/with_progress_bar'
    
    is called. It can also be mixed into other classes, that support the
     #each method protocol.

---
+++ lib/progress_bar/with_progress_bar.rb
@@ -0,0 +1,66 @@
+require 'ruby-progressbar'
+
+class ProgressBar
+  module WithProgressBar
+    def with_progress_bar(options = {}, &block)
+      respond_to?(:each) or raise TypeError, 'object needs to implement the each method'
+      unless options.key?(:total)
+        total_method = [
+          :progress_bar_total, # Define your own total method that is preferredly used
+          :size,
+          :length,
+          :count,
+        ].find { |m| respond_to?(m) }
+        if total_method
+          options[:total] = __send__ total_method
+        end
+      end
+      Thread.current[:__progress_bar__] = ProgressBar.create options
+      if block
+        each do |x|
+          catch :progress_bar_skip do
+            block.call(x)
+            progress_bar.progress_touched? or progress_bar.increment
+            progress_bar.progress_touched = false
+          end
+        end
+        self
+      else
+        begin
+          obj = dup
+        rescue TypeError => e
+          raise e.class, 'Cannot iterate with progress bar over non-duppable objects'
+        end
+        def obj.each(&block)
+          super do |x|
+            catch :progress_bar_skip do
+              block.call(x)
+              progress_bar.progress_touched? or progress_bar.increment
+              progress_bar.progress_touched = false
+            end
+          end
+          self
+        end
+        obj
+      end
+    end
+  end
+
+  module WithProgressBarThreadLocal
+    def progress_bar
+      Thread.current[:__progress_bar__]
+    end
+  end
+end
+
+ObjectSpace.each_object(Module) do |m|
+  if m <= Enumerable
+    m.module_eval do
+      include ProgressBar::WithProgressBar
+    end
+  end
+end
+
+class Object
+  include ProgressBar::WithProgressBarThreadLocal
+end
