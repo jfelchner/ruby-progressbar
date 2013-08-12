3a716f848495cf765de5123d9fee821ef083b85b
Jeff Felchner <accounts+git@thekompanee.com>
2013-08-11 19:37:58 -0500

    Remove depreciation code

--- lib/ruby-progressbar.rb
+++ lib/ruby-progressbar.rb
@@ -1,4 +1,3 @@
-require 'progress_bar/depreciable'
 require 'progress_bar/length_calculator'
 require 'progress_bar/running_average_calculator'
 require 'progress_bar/formatter'
@@ -7,12 +6,6 @@ require 'progress_bar/format'
 require 'progress_bar/base'
 
 class ProgressBar
-  def self.new(*args)
-    puts "DEPRECATION WARNING: Calling `ProgressBar.new` is deprecated and will be removed on or after #{ProgressBar::Depreciable::DEPRECATION_DATE}.  Please use `ProgressBar.create` instead."
-
-    create *args
-  end
-
   def self.create(*args)
     ProgressBar::Base.new *args
   end
