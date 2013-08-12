03aeffedb757e791e1c816744fcfd5446a3b2c87
Jeff Felchner <accounts+git@thekompanee.com>
2013-08-11 19:40:21 -0500

    Rename all of the requires lines to be consistent with the new lib file

--- lib/ruby-progressbar.rb
+++ lib/ruby-progressbar.rb
@@ -1,9 +1,9 @@
-require 'progress_bar/length_calculator'
-require 'progress_bar/running_average_calculator'
-require 'progress_bar/formatter'
-require 'progress_bar/components'
-require 'progress_bar/format'
-require 'progress_bar/base'
+require 'ruby-progressbar/length_calculator'
+require 'ruby-progressbar/running_average_calculator'
+require 'ruby-progressbar/formatter'
+require 'ruby-progressbar/components'
+require 'ruby-progressbar/format'
+require 'ruby-progressbar/base'
 
 class ProgressBar
   def self.create(*args)
