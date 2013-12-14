d727927b4bf86eedbf7c2000ffa0a66aa894b6aa
Jeff Felchner <accounts+git@thekompanee.com>
2013-12-14 16:49:53 -0600

    Whitespace

--- lib/ruby-progressbar/format/base.rb
+++ lib/ruby-progressbar/format/base.rb
@@ -19,9 +19,10 @@ class ProgressBar
         placeholder_length  = remaining_molecules * 2
 
         processed_string.gsub! '%%', '%'
+
         processed_string_length = processed_string.gsub(/\e\[[\d;]+m/, '').length
-        leftover_bar_length = environment.send(:length) - processed_string_length + placeholder_length
-        leftover_bar_length = leftover_bar_length < 0 ? 0 : leftover_bar_length
+        leftover_bar_length     = environment.send(:length) - processed_string_length + placeholder_length
+        leftover_bar_length     = leftover_bar_length < 0 ? 0 : leftover_bar_length
 
         bar_molecules.each do |molecule|
           processed_string.gsub!("%#{molecule.key}", environment.send(molecule.method_name, leftover_bar_length).to_s)
