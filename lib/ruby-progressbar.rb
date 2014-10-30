ac3d3dca82bd5422ffc9d592c3ab8c900b6c0c7f
Jeff Felchner <accounts+git@thekompanee.com>
2014-10-30 17:37:30 -0500

    Refactor: Inline the Formatter module into Base
    This allows us to look at the Base class as a whole and not 'hide' functionality
    which was previously in the formatter.
    
    --------------------------------------------------------------------------------
    Branch:        refactor/massive-update
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>

--- lib/ruby-progressbar.rb
+++ lib/ruby-progressbar.rb
@@ -1,6 +1,5 @@
 require 'ruby-progressbar/length_calculator'
 require 'ruby-progressbar/running_average_calculator'
-require 'ruby-progressbar/formatter'
 require 'ruby-progressbar/components'
 require 'ruby-progressbar/format'
 require 'ruby-progressbar/base'
--- lib/ruby-progressbar/base.rb
+++ lib/ruby-progressbar/base.rb
@@ -2,15 +2,18 @@ require 'ruby-progressbar/length_calculator'
 
 class ProgressBar
   class Base
-    include ProgressBar::Formatter
-
     DEFAULT_OUTPUT_STREAM = $stdout
+    DEFAULT_FORMAT_STRING         = '%t: |%B|'
+    DEFAULT_NON_TTY_FORMAT_STRING = '%t: |%b|'
+    DEFAULT_TITLE                 = 'Progress'
 
     def initialize(options = {})
       self.output       = options[:output] || DEFAULT_OUTPUT_STREAM
       autostart         = options.fetch(:autostart, true)
 
-      super(options)
+      @format_string     = nil
+      self.format_string = options[:format] || DEFAULT_FORMAT_STRING
+      @title             = options[:title]  || DEFAULT_TITLE
 
       @length_calc      = LengthCalculator.new(options)
       @bar              = Components::Bar.new(options)
@@ -106,8 +109,21 @@ class ProgressBar
 
     def title=(title)
       if output.tty?
-        with_update { super }
+        with_update { @title = title }
       end
+    end
+
+    def format(new_format_string = DEFAULT_FORMAT_STRING)
+      self.format_string = new_format_string
+      @format.process(self)
+    end
+
+    def progress
+      @bar.progress
+    end
+
+    def total
+      @bar.total
     end
 
     ###
@@ -214,6 +230,85 @@ class ProgressBar
       else
         stopped? ? "\n" : ""
       end
+    end
+
+    def format_string=(format_string)
+      if @format_string != format_string
+        @format_string = format_string
+        @format        = ProgressBar::Format::Base.new(format_string)
+      end
+    end
+
+    # Format Methods
+    def title
+      @title
+    end
+
+    def percentage
+      @bar.percentage_completed
+    end
+
+    def justified_percentage
+      @bar.percentage_completed.to_s.rjust(3)
+    end
+
+    def percentage_with_precision
+      @bar.percentage_completed_with_precision
+    end
+
+    def justified_percentage_with_precision
+      @bar.percentage_completed_with_precision.to_s.rjust(6)
+    end
+
+    def elapsed_time
+      @elapsed_time
+    end
+
+    def estimated_time_with_no_oob
+      @estimated_time.out_of_bounds_time_format = nil
+      estimated_time
+    end
+
+    def estimated_time_with_unknown_oob
+      @estimated_time.out_of_bounds_time_format = :unknown
+      estimated_time
+    end
+
+    def estimated_time_with_friendly_oob
+      @estimated_time.out_of_bounds_time_format = :friendly
+      estimated_time
+    end
+
+    def bar(length)
+      @bar.length = length
+      @bar.standard_complete_string
+    end
+
+    def complete_bar(length)
+      @bar.length = length
+      @bar.to_s
+    end
+
+    def incomplete_space(length)
+      @bar.length = length
+      @bar.empty_string
+    end
+
+    def bar_with_percentage(length)
+      @bar.length = length
+      @bar.integrated_percentage_complete_string
+    end
+
+    def estimated_time
+      finished? ? @elapsed_time : @estimated_time
+    end
+
+    def rate_of_change
+      @rate.to_s
+    end
+
+    def rate_of_change_with_precision
+      @rate.to_s("%.2f")
     end
   end
 end
