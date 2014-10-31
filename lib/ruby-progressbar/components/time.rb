a374ce2aed135fe647dcc8689fd1f880dd993f68
Jeff Felchner <accounts+git@thekompanee.com>
2014-10-31 18:51:33 -0500

    Style: Make whitespace changes

--- lib/ruby-progressbar/components/time.rb
+++ lib/ruby-progressbar/components/time.rb
@@ -55,7 +55,7 @@ class ProgressBar
       def estimated
         return '??:??:??' if progress.unknown? || timer.stopped?
 
-        hours, minutes, seconds = *timer.divide_seconds(estimated_seconds_remaining)
+        hours, minutes, seconds = timer.divide_seconds(estimated_seconds_remaining)
 
         if hours > 99 && out_of_bounds_time_format
           out_of_bounds_time
@@ -77,7 +77,7 @@ class ProgressBar
       end
 
       def estimated_seconds_remaining
-        (timer.elapsed_seconds * (progress.total / progress.running_average  - 1)).round
+        (timer.elapsed_seconds * (progress.total / progress.running_average - 1)).round
       end
 
       def out_of_bounds_time
