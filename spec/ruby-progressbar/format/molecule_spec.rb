a08fabeb0a6b3943a8f301cf952251d3565df572
Jeff Felchner <accounts+git@thekompanee.com>
2014-11-03 16:24:34 -0600

    Refactor: Update tests for 'Molecule' to the new format
    --------------------------------------------------------------------------------
    Branch:        refactor/massive-update
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>

--- spec/ruby-progressbar/format/molecule_spec.rb
+++ spec/ruby-progressbar/format/molecule_spec.rb
@@ -1,22 +1,30 @@
 require 'rspectacular'
+require 'ruby-progressbar/format/molecule'
 
-describe ProgressBar::Format::Molecule do
-  describe '#new' do
-    before { @molecule = ProgressBar::Format::Molecule.new('t') }
+class     ProgressBar
+module    Format
+describe  Molecule do
+  it 'sets the key when initialized' do
+    molecule = Molecule.new('t')
 
-    it 'sets the key when initialized' do
-      expect(@molecule.key).to eql 't'
-    end
-
-    it 'sets the method name when initialized' do
-      expect(@molecule.method_name).to eql [:title_comp, :title]
-    end
+    expect(molecule.key).to eql 't'
   end
 
-  describe '#bar_molecule?' do
-    it "is true if the molecule's key is a representation of the progress bar graphic" do
-      molecule = ProgressBar::Format::Molecule.new('B')
-      expect(molecule).to be_bar_molecule
-    end
+  it 'sets the method name when initialized' do
+    molecule = Molecule.new('t')
+
+    expect(molecule.method_name).to eql [:title_comp, :title]
   end
+
+  it 'can retrieve the full key for itself' do
+    molecule = Molecule.new('t')
+
+    expect(molecule.full_key).to eql '%t'
+  end
+
+  it 'can determine if it is a bar molecule' do
+    expect(Molecule.new('B')).to be_bar_molecule
+  end
+end
+end
 end
