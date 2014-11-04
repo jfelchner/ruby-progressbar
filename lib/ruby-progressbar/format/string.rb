ff9efa29235bdddb54bb4733950454ed3cd372dd
Jeff Felchner <accounts+git@thekompanee.com>
2014-11-04 17:54:16 -0600

    Refactor: Extract logic out of 'Format::String' that didn't belong there
    I got a bit too carried away with extracting stuff into the `Format::String`.
    Leaving this logic in `Format::Formatter` makes more sense and leads to
    performance improvements.
    
    --------------------------------------------------------------------------------
    Branch:        refactor/massive-update
    Signed-off-by: Jeff Felchner <accounts+git@thekompanee.com>

--- lib/ruby-progressbar/format/string.rb
+++ lib/ruby-progressbar/format/string.rb
@@ -1,48 +1,15 @@
 class   ProgressBar
 module  Format
 class   String < ::String
+  MOLECULE_PATTERN = /%[a-zA-Z]/
   ANSI_SGR_PATTERN = /\e\[[\d;]+m/
 
-  # rubocop:disable Style/MultilineBlockChain, Style/MultilineOperationIndentation
-  def each_molecule
-    dup.
-    each_non_bar_molecule do |molecule, string|
-      yield molecule, string
-    end.
-    each_bar_molecule do |molecule, string|
-      yield molecule, string
-    end
-  end
-
-  def each_non_bar_molecule
-    non_bar_molecules.each_with_object(self) do |molecule, string|
-      yield molecule, string
-    end.
-    gsub('%%', '%')
-  end
-  # rubocop:enable Style/MultilineBlockChain, Style/MultilineOperationIndentation
-
-  def each_bar_molecule
-    bar_molecules.each_with_object(self) do |molecule, string|
-      yield molecule, string
-    end
-  end
-
-  def length_available_for_bar(max_length)
-    @length_available_for_bar ||= \
-      begin
-        bar_length = max_length - displayable_length + molecule_placeholder_length
-
-        bar_length < 0 ? 0 : bar_length
-      end
-  end
-
   def displayable_length
-    @displayable_length ||= gsub(ANSI_SGR_PATTERN, '').length
+    gsub(ANSI_SGR_PATTERN, '').length
   end
 
-  def molecule_placeholder_length
-    @molecule_placeholder_length ||= scan(/%[a-zA-Z]/).size * 2
+  def bar_molecule_placeholder_length
+    @bar_molecule_placeholder_length ||= bar_molecules.size * 2
   end
 
   def non_bar_molecules
@@ -57,7 +24,7 @@ class   String < ::String
     @molecules ||= begin
                       molecules = []
 
-                      scan(/%[a-zA-Z]/) do |match|
+                      scan(MOLECULE_PATTERN) do |match|
                         molecules << Molecule.new(match[1, 1])
                       end
 
