d074efc0748fc10952c603dc9648dd2835d08191
Jeff Felchner <accounts+git@thekompanee.com>
2016-04-23 16:48:23 -0500

    Changed: ProgressBar::Time to an instantiated class
    Having this be a class and also storing state on it made this difficult to test.
    Now that we're only instantiating one of these per bar, there's not really any
    reason this can't be instantiated.

--- spec/ruby-progressbar/time_spec.rb
+++ spec/ruby-progressbar/time_spec.rb
@@ -1,17 +1,14 @@
 require 'spec_helper'
 
 class TimeMockedWithTimecop
-  def self.now; end
   def self.now_without_mock_time; end
 end
 
 class TimeMockedWithDelorean
-  def self.now; end
   def self.now_without_delorean; end
 end
 
 class TimeMockedWithActiveSupport
-  def self.now; end
   def self.__simple_stub__now; end
 end
 
@@ -21,39 +18,28 @@ end
 
 class     ProgressBar
 describe  Time do
-  # To test unmocked method selection for real, we have to clear the memoized
-  # variable. But this breaks other examples, because now the wrong method is being memoized. So we restore the previos method in +after+ hook
-  before do
-    @old_unmocked_time_method = Time.instance_variable_get(:@unmocked_time_method)
-    Time.instance_variable_set(:@unmocked_time_method, nil)
-  end
-
-  after do
-    Time.instance_variable_set(:@unmocked_time_method, @old_unmocked_time_method)
-  end
-
   it 'when Time is being mocked by Timecop retrieves the unmocked Timecop time' do
     expect(TimeMockedWithTimecop).to receive(:now_without_mock_time).once
 
-    Time.now(TimeMockedWithTimecop)
+    Time.new(TimeMockedWithTimecop).now
   end
 
   it 'when Time is being mocked by Delorean retrieves the unmocked Delorean time' do
     expect(TimeMockedWithDelorean).to receive(:now_without_delorean).once
 
-    Time.now(TimeMockedWithDelorean)
+    Time.new(TimeMockedWithDelorean).now
   end
 
   it 'when Time is being mocked by ActiveSupport retrieves the unmocked time' do
     expect(TimeMockedWithActiveSupport).to receive(:__simple_stub__now).once
 
-    Time.now(TimeMockedWithActiveSupport)
+    Time.new(TimeMockedWithActiveSupport).now
   end
 
   it 'when Time is not being mocked will return the actual time' do
     expect(UnmockedTime).to receive(:now).once
 
-    Time.now(UnmockedTime)
+    Time.new(UnmockedTime).now
   end
 end
 end
--- spec/support/time.rb
+++ spec/support/time.rb
@@ -10,12 +10,8 @@ end
 
 class ProgressBar
 class Time
-  class << self
-    alias_method :old_now, :now
-
-    def now(time = ::PBTimeTester)
-      old_now(time)
-    end
+  def initialize(time = ::PBTimeTester)
+    self.time = time
   end
 end
 end
