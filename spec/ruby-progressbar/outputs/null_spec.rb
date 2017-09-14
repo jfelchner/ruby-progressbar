2bc0a4a83f3636c8bb5f3d5d4c2fa5fc14ce6fd4
Jeff Felchner <catchall+git@jefffelchner.com>
2017-09-14 04:30:59 -0500

    Add: Outputs::Null for users who don't want the bar sent anywhere

---
+++ spec/ruby-progressbar/outputs/null_spec.rb
@@ -0,0 +1,26 @@
+require 'rspectacular'
+require 'ruby-progressbar/outputs/null'
+
+class    ProgressBar
+module   Outputs
+describe Null do
+  let(:output_io) { StringIO.new }
+
+  it 'does not output anything ever' do
+    allow_any_instance_of(Null).to receive(:stream).and_return(output_io)
+
+    progressbar = ProgressBar::Base.new(:length => 20, :output => Null)
+
+    progressbar.increment
+    progressbar.log('hello')
+    progressbar.progress += 20
+    progressbar.reset
+    progressbar.finish
+
+    output_io.rewind
+
+    expect(output_io.read).to be_empty
+  end
+end
+end
+end
