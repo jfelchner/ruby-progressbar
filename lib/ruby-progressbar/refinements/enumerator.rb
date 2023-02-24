2616f9968e6ebf8ff2cac2d2f0c5d81e488ebb22
Jeff Felchner <catchall+git@jefffelchner.com>
2023-02-24 17:19:14 -0600

    Add: Ability To Access Progress Bar Instance Using Enumerator Refinement
    Why This Change Is Necessary
    ========================================================================
    
    Without access to the progress bar instance, when using the refinement
    there's a lack of ability to do things like log or modify the title
    mid-progress like you can with the standard progress bar usage.
    
    What These Changes Do To Address the Issue
    ========================================================================
    
    If you pass a second argument to the block passed to `with_progressbar`,
    the second argument will be the progress bar instance.
    
    Side Effects Caused By This Change
    ========================================================================
    
    None expected.
    
    ------------------------------------------------------------------------
    
    **Relevant URLs:**
    
      * [Current Ticket Link][1]
      * [Original Ticket Link][2]
    
    **Actions:**
    
      * Closes 184
      * Closes 185
    
    ------------------------------------------------------------------------
    
    [1]: https://github.com/jfelchner/ruby-progressbar/issues/185
    [2]: https://github.com/jfelchner/ruby-progressbar/issues/184

--- lib/ruby-progressbar/refinements/enumerator.rb
+++ lib/ruby-progressbar/refinements/enumerator.rb
@@ -1,22 +1,28 @@
 class  ProgressBar
 module Refinements
 module Enumerator
-refine ::Enumerator do
-  def with_progressbar(options = {}, &block)
-    progress_bar = ProgressBar.create(options.merge(:starting_at => 0, :total => size))
+  ARITY_ERROR_MESSAGE = 'Only two arguments allowed to be passed to ' \
+                        'with_progressbar (item, progress_bar)'.freeze
 
-    each do |item|
-      progress_bar.increment
+  refine ::Enumerator do
+    def with_progressbar(options = {}, &block)
+      progress_bar = ProgressBar.create(options.merge(:starting_at => 0, :total => size))
 
-      next unless block
+      each do |item|
+        progress_bar.increment
 
-      yielded_args = []
-      yielded_args << item
+        next unless block
 
-      yield(*yielded_args)
+        yielded_args = []
+        yielded_args << item         if block.arity > 0
+        yielded_args << progress_bar if block.arity > 1
+
+        fail ::ArgumentError, ARITY_ERROR_MESSAGE if block.arity > 2
+
+        yield(*yielded_args)
+      end
     end
   end
-end
 end
 end
 end
