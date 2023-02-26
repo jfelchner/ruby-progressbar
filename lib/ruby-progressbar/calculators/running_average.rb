1cfc12b7384648cc71e437ba43eb788db091a6fb
Jeff Felchner <catchall+git@jefffelchner.com>
2023-02-25 23:51:08 -0600

    Change: Rename smoothing_factor To rate

--- lib/ruby-progressbar/calculators/running_average.rb
+++ lib/ruby-progressbar/calculators/running_average.rb
@@ -1,9 +1,8 @@
 class   ProgressBar
 module  Calculators
 class   RunningAverage
-  def self.calculate(current_average, new_value_to_average, smoothing_factor)
-    (new_value_to_average * (1.0 - smoothing_factor)) +
-      (current_average * smoothing_factor)
+  def self.calculate(current_average, new_value_to_average, rate)
+    (new_value_to_average * (1.0 - rate)) + (current_average * rate)
   end
 end
 end
